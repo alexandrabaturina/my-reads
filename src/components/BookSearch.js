9687e014f8ab8b5a9bf822246cb16fa1224b3d0b
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-08-30 15:09:29 -0700

    feat: update search query

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -1,19 +1,59 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import * as BooksAPI from '../utils/BooksAPI.js'
+import Book from './Book'
 
-const BookSearch = () => {
+const BookSearch = ({ books, onChangeBookshelf }) => {
+
+    const [query, setQuery] = useState('')
+    const [searchResults, setSearchResults] = useState([])
+
+    const searchBooks = q => {
+        BooksAPI.search(q)
+            .then(res => {
+                if (res) {
+                    if (res.error) {
+                        setSearchResults([])
+                    } else {
+                        setSearchResults(res)
+                    }
+                } else {
+                    console.log('Not found')
+                }
+            })
+
+    }
+
+    const updateQuery = e => {
+        setQuery(e.target.value)
+        searchBooks(e.target.value)
+    }
+
     return (
         <div className="search-books">
             <div className="search-books-bar">
                 <Link to='/' className="close-search">Close</Link>
                 <div className="search-books-input-wrapper">
-                    <input type="text" placeholder="Search by title or author" />
-                    <div className="search-books-results">
-                        <ol className="books-grid"></ol>
-                    </div>
+                    <input
+                        type="text"
+                        placeholder="Search by title or author"
+                        value={query}
+                        onChange={updateQuery} />
                 </div>
             </div>
-        </div>
-    )
+            <div className="search-books-results">
+                <ol className="books-grid">
+                    {searchResults && searchResults.map(book => (
+                        <li key={book.id}>
+                            <Book
+                                book={book}
+                                onChangeBookshelf={onChangeBookshelf} />
+                        </li>
+                    ))}
+
+                </ol>
+            </div>
+        </div>)
 }
 
 export default BookSearch
